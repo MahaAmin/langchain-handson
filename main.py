de6ad26a400e50a8488321b4e0b9b40154e7e168
MahaAmin <mahaamin97@gmail.com>
2026-07-06 15:05:22 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_ollama import ChatOllama
+
+load_dotenv()
+
+
+def main():
+    print("Hello world from langchain!")
+
+
+if __name__ == "__main__":
+    main()
